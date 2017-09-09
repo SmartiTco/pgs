8c1f9b35b762b76fbb99c07f17c9170fda6978f9
Unknown <vanjablou@gmail.com>
2017-09-09 15:38:08 +0300

    Auto stash before merge of "dev_hades" and "master"
    #add image in board

--- pegasus/static/js/board-elements.js
+++ pegasus/static/js/board-elements.js
@@ -197,3 +197,204 @@ var TextElement = function(softID, canvasID, boardID, invite, canEdit){ // canEd
 		}
 	}
 };
+
+//IMAGE
+
+var ImageElement = function(softID, canvasID, boardID, invite, canEdit){ // canEdit is boolean
+	this.content = '';
+	this.softID = softID;
+	this.canvasID = canvasID;
+	this.boardID = boardID;
+	this.invite = invite;
+	this.elementID = ''; // empty for now
+	this.editable = canEdit;
+	this.position = JSON.stringify({"top": 0, "left": 0});
+	this.init = function(message, id, pos){
+		var self = this;
+		self.elementID = id;
+		self.position = pos;
+		var jsonParse = JSON.parse(self.position);
+		$(self.canvasID).prepend('<span class="draggable-image" id="image-'+self.elementID+'"></span>');
+		var list = message.split('\n');
+		var final = list.join('<br>');
+		self.content = final;
+		$('#image-'+self.elementID).html(final);
+		$('#image-'+self.elementID).css(jsonParse);
+		if(self.editable){
+			$('#image-' + self.elementID).draggable({
+				containment: self.canvasID,
+				cursor: 'move',
+				stop: function(event, ui){
+					self.refresh(ui.position);
+				}
+			});
+			function hoverEnterEvent(){
+				$('#image-delete-form-'+self.elementID).remove();
+				$(this).prepend('<form class="delete-form" id="image-delete-form-'+self.elementID
+							+'"><button type="submit" class="global-btn delete-btn" title="Delete Image" id="delete-'+self.elementID
+							+'"><i class="fa fa-trash"></i></button></form>');
+				$('#image-delete-form-'+self.elementID).bind('submit', function(event){
+					event.preventDefault();
+					var id = self.elementID;
+					var csrf = $("input[name='_csrf_token']").val();
+					var invite = self.invite;
+					var url = Flask.url_for('delete_component', {boardID:self.boardID, componentID:id});
+					$.ajax({
+						async: false,
+						method: 'POST',
+						url: url,
+						data: {
+							'_csrf_token': csrf,
+							'invite': invite
+						},
+						success: function(data){
+							if(data.error == 'None'){
+								$('#image-' + self.elementID).remove();
+							}
+							else{
+								$(self.canvasID).prepend('<div id="board-error" class="alert alert-warning">'
+												+'<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
+												+'<strong>Error: </strong>'+data.error+'</div>');
+							}
+							$("input[name='_csrf_token']").val(data.token);
+						}
+					});
+				});
+			}
+			function hoverLeaveEvent(){
+				$('#image-delete-form-'+self.elementID).remove();
+			}
+			function doubleClickEvent(){
+				var v = $(this).image(); // this won't render line breaks so fix it
+				$(this).html('<form id="image-edit-form-'+self.elementID
+							+'"><textarea id="image-edit-'+self.elementID
+							+'" name="message" class="image-edit-input">'+v+'</textarea></form>');
+				$('#image-edit-'+self.elementID).focus();
+				$('#image-edit-'+self.elementID).bind('blur', function(){
+					$('#image-edit-form-'+self.elementID).submit();
+				});
+				$('#image-edit-'+self.elementID).keydown(function(event){
+					if(event.keyCode==13 && !event.shiftKey){
+						$('#image-edit-form-'+self.elementID).submit();
+						return false;
+					}
+				});
+				$('#image-edit-form-'+self.elementID).bind('submit', function(event){
+					event.preventDefault();
+					var val = $('#image-edit-'+self.elementID).val();
+					self.save(val, 'edit', self.elementID);
+				});
+
+			}
+			$('#image-' + self.elementID).dblclick(doubleClickEvent);
+			// double tap event would go here
+
+			$('#image-' + self.elementID).hover(hoverEnterEvent, hoverLeaveEvent);
+			// single tap event would go here
+		}
+
+	}
+	this.create = function(){
+		var self = this;
+		if(self.editable){
+			$(self.canvasID).prepend('<form id="image-create-form-'+self.softID+'">'
+								+'<imput type="file" name="image" id="image-create-'+self.softID+'" class="image-create-input"></imput>'
+								+'</form>');
+			$('#image-create-'+self.softID).focus();
+			$('#image-create-'+self.softID).bind('blur', function(){
+				$('#image-create-form-'+self.softID).submit();
+			});
+			$('#image-create-'+self.softID).keydown(function(event){
+				if(event.keyCode==13 && !event.shiftKey){
+						$('#image-create-form-'+self.softID).submit();
+						return false;
+					}
+			});
+			$('#image-create-form-'+self.softID).bind('submit', function(event){
+				event.preventDefault();
+				var val = $('input[name="image"]', this).val();
+				self.save(val, 'create', self.softID);
+			});
+		}
+		
+	}
+	this.save = function(image, type, id, pos){
+		var self = this;
+		if(self.editable){
+			var URL;
+			if(type=='create'){
+				URL = Flask.url_for('post_components', {boardID:self.boardID});
+				position = self.position;
+			}
+			else{
+				URL = Flask.url_for('edit_component', {boardID:self.boardID, componentID:id});
+				position = ''; // won't actually be handled in any way server-side since this 'hasMessages'
+			}
+
+			$.ajax({
+				method: 'POST',
+				async: false,
+				url: URL,
+				data:{
+					'_csrf_token': $("input[name='_csrf_token']").val(),
+					'invite': self.invite,
+					'content-type': 'image',//#image content type
+					'hasImages': 'true',
+					'image': image,
+					'position': position
+				},
+				success: function(data){
+					if(data.error == 'None'){
+						var formSelector = '#image-'+type+'-form-'+id;
+						$(formSelector).remove();
+						if(type=='create'){
+							self.init(message, data.componentID, position);
+						}
+						else{
+							var list = message.split('\n');
+							var final = list.join('<br>');
+							$('#image-'+self.elementID).html(final);
+							self.content = final;
+						}
+					}
+					else{
+						$(self.canvasID).prepend('<div id="board-error" class="alert alert-warning">'
+												+'<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
+												+'<strong>Error: </strong>'+data.error+'</div>');
+					}
+					$("input[name='_csrf_token']").val(data.token);
+				}
+			});
+		}	
+	}
+	this.refresh = function(position){
+		var self = this;
+		if(self.editable){
+			var posToString = JSON.stringify(position);
+			self.position = posToString;
+			$.ajax({
+				method: 'POST',
+				url: Flask.url_for('edit_component', {boardID:self.boardID, componentID:self.elementID}),
+				async: false,
+				data:{
+					'_csrf_token': $("input[name='_csrf_token']").val(),
+					'invite': self.invite,
+					'content-type': 'image', // #image content type
+					'hasMessages': 'false',
+					'position': posToString
+				},
+				success: function(data){
+					if(data.error == 'None'){
+						console.log("Successfully updated position!");
+					}
+					else{
+						$(self.canvasID).prepend('<div id="board-error" class="alert alert-warning">'
+												+'<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>'
+												+'<strong>Error: </strong>'+data.error+'</div>');
+					}
+					$("input[name='_csrf_token']").val(data.token);
+				}
+			});
+		}
+	}
+};
